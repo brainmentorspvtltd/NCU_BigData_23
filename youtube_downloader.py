bd9a2a365dab7268473df904a5be1fff45b62044
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-07-13 16:23:29 +0530

    Add files via upload

---
+++ youtube_downloader.py
@@ -0,0 +1,57 @@
+import pickle
+import os
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+    credentials = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    #  Check if the credentials are invalid or do not exist
+    if not credentials or not credentials.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES)
+        credentials = flow.run_console()
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
+
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+def get_video_comments(service, **kwargs):
+    comments = []
+    results = service.commentThreads().list(**kwargs).execute()
+
+    while results:
+        for item in results['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments.append(comment)
+
+        # Check if another page exists
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = service.commentThreads().list(**kwargs).execute()
+        else:
+            break
+
+    return comments
+
+service = get_authenticated_service()
+video_id = '4jnQdeFigTg'
+comments = get_video_comments(service, part='snippet',
+                              videoId=video_id,
+                              textFormat='plainText')
+print(comments)
+
+
+
+
